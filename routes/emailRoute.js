2969d46d85b3c452f446ecf135453938a011aaa0
Debanjan <DS098199@cerner.net>
2024-01-29 00:18:27 +0530

    Made SMTP passwords and sender information fetch from UI

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -1,33 +1,26 @@
 import nodemailer from 'nodemailer';
 import fs from 'fs';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
-  auth: {
-    user: process.env.SMTP_MAIL,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+
 
 const emailRoute = (req, res) => {
-  const { data } = req.body; // Here we have all excel data
-  if( data && data.length > 0){// Now we traverse, and go to every data set/rows, and store them
-    data.forEach((individualEmailData) => {
-        const { company_name, email_of_employees, role } = individualEmailData;
-        mailParams(company_name, email_of_employees, role);
-    })
-  }
-};
-const mailParams = (company_name, email_of_employees, role) => {
-//   company_name, email_of_employees, role, name, yoe, subject
-  let emailBody = `Hello, I hope this message finds you well.
-                    I am writing to express my interest in the ${role} position at ${company_name}.
-                    I believe my skills and experience align well with the requirements of the role.
-                    I have a background in Java and Spring Boot. I have attached my resume for your consideration.
-                    Thank you for considering my application. I look forward to the opportunity to speak with you.`;
-
+  const { name, password, sender_mail, yoe, currentCompany, data } = req.body; // Here we have all excel data & password
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: sender_mail,
+      pass: password,
+    },
+  });
+  transporter.verify(function (error, success) {
+    if (error) {
+      console.log('SMTP Server connection error:', error);
+    } else {
+      console.log('SMTP Server connection is ready',success);
+    }
+  });
   // Get list of uploaded resumes
   const resumesPath = './uploads/';
   const attachments = fs.readdirSync(resumesPath).map((filename) => ({
@@ -35,8 +28,27 @@ const mailParams = (company_name, email_of_employees, role) => {
     path: `${resumesPath}${filename}`,
   }));
 
+  if( data && data.length > 0){// Now we traverse, and go to every data set/rows, and store them
+    mailParams(data, attachments, transporter, name, sender_mail, yoe, currentCompany);
+  }
+  
+    // deleteAttachments(attachments);// Delete all the attachments from ./uploads folder
+};
+const mailParams = (data, attachments, transporter, name, sender_mail, yoe, currentCompany) => {
+
+  data.forEach((individualEmailData) => {
+  const { company_name, email_of_employees, role } = individualEmailData;
+
+  let emailBody = `Hello, my name is ${name}. I hope this message finds you well.
+                    I am writing to express my interest in the ${role} position at ${company_name}.
+                    I believe my skills and experience align well with the requirements of the role.
+                    Currently I am working in ${currentCompany} with ${yoe} Years of experience.
+                    I have a background in Java and Spring Boot. I have attached my resume for your consideration.
+                    Thank you for considering my application. I look forward to the opportunity to speak with you.`;
+
+
   let mailOptions = {
-    from: process.env.SMTP_MAIL,
+    from: sender_mail,
     to: email_of_employees,
     subject: company_name,
     text: emailBody,
@@ -48,9 +60,9 @@ const mailParams = (company_name, email_of_employees, role) => {
       console.log(error);
     } else {
       console.log("Email sent successfully!");
-      deleteAttachments(attachments);
     }
   });
+})
 };
 
 // Remove uploaded resumes after successfully sending the email
