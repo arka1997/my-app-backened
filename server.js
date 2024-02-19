30e41b79d53c68717e94ef86c5fd70066c244f92
Debanjan <DS098199@cerner.net>
2024-02-19 14:03:43 +0530

    Added Logges to track requests

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ import { resumeRoute } from './routes/resumeRoute.js';
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+    console.log(`Received request: ${req.method} ${req.url}`);
+    next();
+  });
 // Use of Express: serve when requests(GET,POST) comes and route them to Database, and lsiten them, and return response.
 // Here listening to a get request, and returning the response fetched to client.
 // there is a callback route -> '/' OR '/getRequest/1', and in browser we select this route, and we hit this API call
@@ -19,8 +22,8 @@ app.get('/', (req, res) => {
 });
 
 app.post('/uploadResume', (req, res) => {
-    resumeRoute(req, res);   
-    
+    resumeRoute(req, res); 
+    res.sendStatus(200);
 });
 
 app.post('/excelUpload',(req,res) => {
@@ -31,5 +34,5 @@ app.post('/excelUpload',(req,res) => {
 const port = process.env.PORT || 3001;// Either get the Environment variable in production level Server, or run the default port
 // Now listening to the new port created
 app.listen(port, () => {
-    console.log(`Serve at https://expressflash.netlify.app/:${port}`);
+    console.log(`Serve at https://expressflash.onrender.com/:${port}`);
 })
